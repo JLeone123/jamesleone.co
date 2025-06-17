7bb2efe9c4bf2ff7844e5f8fcefa9bcb4bf5ab05
James Leone <jsalleo13@gmail.com>
2025-06-16 22:09:18 -0400

    Redesign hero section, and add navbar and spotlight

---
+++ components/contact-header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactHeader = () => {
+  return (
+    <div className="">
+      <h2>I would love to work with you! Let's get in touch! </h2>
+    </div>
+  );
+};
+
+export default ContactHeader;
---
+++ components/floating-navbar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconUser,
+  IconTerminal2,
+} from "@tabler/icons-react";
+
+export function FloatingNavbar() {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-blue-400 dark:text-blue-400" />
+      ),
+      href: "#home",
+    },
+
+    {
+      title: "Projects",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-pink-400 dark:text-pink-400" />
+      ),
+      href: "#projects",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub
+          className="h-full w-full text-green-400 dark:text-green-400"
+          target="_blank"
+        />
+      ),
+      href: "https://www.github.com/JLeone123",
+    },
+    {
+      title: "Contact",
+      icon: (
+        <IconUser className="h-full w-full text-green-400 dark:text-cyan-400" />
+      ),
+      href: "#contact",
+    },
+  ];
+  return (
+    <div className="flex mt-15 justify-center w-full">
+      <FloatingDock mobileClassName="" items={links} />
+    </div>
+  );
+}
